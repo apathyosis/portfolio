f5b6a4a8828147236018e0019bb96f68523ef50d
Theo <cyclical.eclipsed@gmail.com>
2025-02-11 17:55:07 -0500

    attempt two for vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: "/portfolio/",
+
     build: {
+        outDir: "docs",
         rollupOptions: {
             input: {
                 main: '/index.html',
